48319348df80295a95083f5ccf9ced3618758823
JCB1995 <33055099+JCB1995@users.noreply.github.com>
2019-05-01 15:24:52 +0100

    iokio

--- LudumDare44Game/Assets/Scripts/Coin.cs
+++ LudumDare44Game/Assets/Scripts/Coin.cs
@@ -6,10 +6,13 @@ public class Coin : MonoBehaviour
 {
     public int coinValue = 0;
 
+    public GameObject pickupSound;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Instantiate(pickupSound, transform.position, Quaternion.identity);
             ScoreManager.instance.ChangeScore(coinValue);
         }
     }
